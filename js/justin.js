30a99f39d515aa15f6f2f76c1412cde177e75758
Zak Spence <zakspence@gmail.com>
2017-01-25 11:37:24 -0600

    collated translate functions.

--- js/justin.js
+++ js/justin.js
@@ -2,26 +2,35 @@
 
 var Translator = (function( ){
 
-	var french = {
-		"merry":"Meey",
-		"christmas":"Navidad",
-		"and":"y",
-		"happy":"contento",
-		"new":"nuevo",
-		"year":"año"
+	var spanish = {
+		"merry":"frohe", 
+		"christmas":"weihnachten", 
+		"and":"und",
+		"happy":"gutes", 
+		"new":"neues", 
+		"year":"Jahr"
 	}
 
 	var spanishKeys = Object.keys(spanish);
-
+	var spanishValues = Object.values(spanish);
+	
 	 return {
-	 	translateToSpanish : function(array){
-	 		console.log('translating...', array, frenchKeys)
-			for( var i = 0; i < spanishKeys.length; i++ ){
-				if (array[array.length - 2] === spanishKeys[i]) {
-					console.log(spanishKeys[i], spanish[spanishKeys[i]])
-					return spanish[spanishKeys[i]];
+	 	translateToGerman : function(array ){
+
+			var outPut = array.slice(0, -1);
+
+			// this loop controls which input word we are comparing to our dictionary
+			for (var q = 0; q < outPut.length; q++ ){
+				//this loop compares the input word to all words in our dictionary
+				for (var i = 0; i < spanishKeys.length; i++){
+					if (outPut[q] === spanishKeys[i] ){
+						outPut[q] = spanishValues[i];
+					}
 				}
 			}
+			outPut = outPut.join(" ");
+			console.log("outPUT: ", outPut);
+			return outPut;
 		}
 	}
 })();
